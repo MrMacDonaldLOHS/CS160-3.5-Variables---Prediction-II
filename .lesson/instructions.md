766ef15436cfd2c9c15db6fe4163366203042320
mrmacdonald <96990486+MrMacDonaldLOHS@users.noreply.github.com>
2022-02-21 17:17:10 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,23 @@
+# Task - Investigate
+
+Before running, fill in the comments to predict how the code will work, and answer the other questions.
+
+As always **you will not lose points for having the wrong answer.** Being wrong and then being right is how you learn. 
+
+If there are any errors in the code, correct them so that it runs cleanly.
+
+### What is assignment?
+
+Need more help with assignment? [click here](https://www.w3schools.com/python/python_variables.asp)
+
+### What is concatenation?
+
+Need more help with concatenation? [click here](https://www.w3schools.com/python/gloss_python_string_concatenation.asp)
+
+## Help! My Code Doesn't Work!
+
+Make sure that you check for the following things:
+
+- print does not have a capital letter
+- The string text is surrounded with quotation marks
+- The string (including quotation marks) is surrounded by parenthesis.
---
+++ main.py
@@ -0,0 +1,53 @@
+### Example Code 1
+
+fName = "Mr."
+lName = "Mac Donald"
+#lName = "Magoo"
+print(fName + " " + lName)
+# Predict: If we remove the comment from line 5, what will be printed?
+# Prediction:
+
+# Identify a variable in the code on line 6
+# Answer:
+
+# Identify a string in the code on line 6
+# Answer:
+
+### Example Code 2
+
+num1 = 20
+num2 = 5
+
+total1 = num1 + 15
+total2 = num2 * 2
+total3 = num1 - num2
+
+print(total3)
+#Predict the output:
+
+total3 = total3/2
+
+print(total3)
+#Predict the output:
+
+
+### Example Code 3
+
+name1 = "Phineas" 
+name2 = "Perry" 
+name3 = "Ferb"  
+
+print(name1 + " and " + name3) 
+#Predict the output:
+print("Curse you " + name3 + " the Platypus!") 
+#Predict the output: 
+name3 = "Doofenshmirtz" 
+Print("Curse you " + name3 + " the Platypus!") 
+#Predict the output: 
+
+### Example Code 4
+num1 = "1"
+num2 = "2"
+num3 = num1 + num2
+print(num3)
+#Predict the output:
